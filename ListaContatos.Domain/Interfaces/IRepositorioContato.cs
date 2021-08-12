042c2d2a9c6ab5d4014b49da847a2885db7c46fd
Allerson <allerson.santana354@gmail.com>
2021-08-12 15:04:15 -0300

    Criando a interface IRepositorioContato

---
+++ ListaContatos.Domain/Interfaces/IRepositorioContato.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace ListaContatos.Domain.Interfaces
+{
+    public interface IRepositorioContato<T>
+    {
+        List<T> Listar();
+
+        void Cadastrar(T entidade);
+
+        void Excluir(int Id);
+
+        T RetornarPorId(int Id);
+
+        void Atualizar(int Id, T entidade);
+    }
+}

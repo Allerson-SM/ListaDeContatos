7138049f42044a1f8ab97b745b7e9e61abd677c0
Allerson <allerson.santana354@gmail.com>
2021-08-12 15:03:47 -0300

    Criando a classe ContatoRepositorio

---
+++ ListaContatos.Domain/Model/ContatoRepositorio.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using ListaContatos.Domain.Interfaces;
+
+namespace ListaContatos.Domain.Model
+{
+    public class PessoaRepositorio : IRepositorioContato<Contato>
+    {
+        private List<Contato> ListaContato = new List<Contato>();
+
+        public void Atualizar(int Id, Contato entidade)
+        {
+            ListaContato[Id] = entidade;
+        }
+
+        public void Cadastrar(Contato entidade)
+        {
+            ListaContato.Add(entidade);
+        }
+
+        public void Excluir(int Id)
+        {
+            Id = Id - 1;
+            ListaContato.RemoveAt(Id);
+        }
+
+        public List<Contato> Listar()
+        {
+            return ListaContato;
+        }
+
+        public Contato RetornarPorId(int Id)
+        {
+            return ListaContato[Id];
+        }
+    }
+}

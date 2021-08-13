6ce17a81ff8c6cda36c1736ab489db495a27bcc0
Allerson <allerson.santana354@gmail.com>
2021-08-12 23:27:39 -0300

    Implementado métodos para utilizar no program

--- ListaContatos.ConsoleUI/Services/ServiceContato.cs
+++ ListaContatos.ConsoleUI/Services/ServiceContato.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using ListaContatos.Domain.Model;
 
 namespace ListaContatos.ConsoleUI.Services
@@ -9,6 +10,43 @@ namespace ListaContatos.ConsoleUI.Services
         public ServiceContato(ContatoRepositorio repositorio)
         {
             _repositorio = repositorio;
+        }
+
+        public List<Contato> Listar()
+        {
+            return _repositorio.Listar();
+            
+        }
+
+        public void Cadastrar(string nome, int idade, string email)
+        {
+            var id = _repositorio.ProximoId();
+
+            Contato novoContato = new Contato(id,
+                                              nome,
+                                              idade,
+                                              email);
+
+            _repositorio.Cadastrar(novoContato);
+        }
+
+        public void Excluir(int id)
+        {
+            _repositorio.Excluir(id);
+        }
+
+        public void Atualizar(int id, string nome, int idade, string email)
+        {
+            Contato contatoAtualizado = new Contato(id,
+                                                    nome,
+                                                    idade,
+                                                    email);
+            _repositorio.Atualizar(id, contatoAtualizado);
+        }
+
+        public Contato Consultar(int id)
+        {
+            return _repositorio.RetornarPorId(id);
         }
 
         

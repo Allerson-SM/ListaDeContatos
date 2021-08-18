8ac44bb34e5a3b1cfbdabb0ddec45d38f0823699
Allerson <allerson.santana354@gmail.com>
2021-08-18 16:06:25 -0300

    Criação da migration

---
+++ ListaContatos.Data/Migrations/20210818185133_ListaContato.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ListaContatos.Data.Migrations
+{
+    public partial class ListaContato : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contato",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contato", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contato");
+        }
+    }
+}

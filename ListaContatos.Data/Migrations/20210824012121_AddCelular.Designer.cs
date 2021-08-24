a1f3d560784f558ce502899e887eed77b597880d
Allerson <allerson.santana354@gmail.com>
2021-08-23 22:31:07 -0300

    Migrations atualizadas com propriedade nova

--- ListaContatos.Data/Migrations/20210824012121_AddCelular.Designer.cs
+++ ListaContatos.Data/Migrations/20210824012121_AddCelular.Designer.cs
@@ -3,14 +3,16 @@ using ListaContatos.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ListaContatos.Data.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20210824012121_AddCelular")]
+    partial class AddCelular
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ListaContatos.Data/Migrations/20210824012121_AddCelular.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ListaContatos.Data.Migrations
+{
+    public partial class AddCelular : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Celular",
+                table: "Contato",
+                type: "varchar(50)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Celular",
+                table: "Contato");
+        }
+    }
+}
